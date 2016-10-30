bea88533cf76941c3f53bb2fc88ab863e6a0003c
wushuang123 <zxj100@foxmail.com>
2016-10-30 23:12:12 +0800

    index

--- src/main/java/com/xifar/console/controller/HomeController.java
+++ src/main/java/com/xifar/console/controller/HomeController.java
@@ -1,5 +1,12 @@
 package com.xifar.console.controller;
 
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,16 +16,10 @@ import org.springframework.web.servlet.ModelAndView;
 public class HomeController {
 
 	@RequestMapping(value = { "/index" })
-	public ModelAndView gotoHome() {
-		ModelAndView mv = new ModelAndView();
-		mv.setViewName("/index");
-		return mv;
+	public void gotoHome(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		RequestDispatcher sss = request.getRequestDispatcher("/WEB-INF/views/html/index.html");
+		sss.forward(request, response);
 	}
 	
-	@RequestMapping(value = { "/demo" })
-	public ModelAndView demo() {
-		ModelAndView mv = new ModelAndView();
-		mv.setViewName("/demo");
-		return mv;
-	}
 }
